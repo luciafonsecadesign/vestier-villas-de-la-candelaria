1556306a354dac958d52686b55baf06ce8555dc4
Jhonatan Osorio <jaosorio1013@gmail.com>
2024-02-16 00:30:06 -0600

    Mejora en 360

--- data.js
+++ data.js
@@ -30,23 +30,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.0122126372031701,
-          "pitch": 0.17146775311513274,
-          "rotation": 0.7853981633974483,
-          "target": "2-vestidos-y-esposo"
+          "yaw": -3.000816215818741,
+          "pitch": 0.2280184465494166,
+          "rotation": 0,
+          "target": "1-zapatero"
         },
         {
-          "yaw": -2.121734562620345,
-          "pitch": 0.2220320407920191,
-          "rotation": 5.497787143782138,
-          "target": "1-zapatero-y-bolsos"
+          "yaw": -2.079173664105225,
+          "pitch": 0.2087772051927459,
+          "rotation": 0.7853981633974483,
+          "target": "2-vestidos-y-esposo"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-zapatero-y-bolsos",
-      "name": "Zapatero y Bolsos",
+      "id": "1-zapatero",
+      "name": "Zapatero",
       "levels": [
         {
           "tileSize": 256,
@@ -74,14 +74,14 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.9944177186662628,
-          "pitch": 0.2534370969319273,
-          "rotation": 5.497787143782138,
+          "yaw": -2.974708502950566,
+          "pitch": 0.1257580464916419,
+          "rotation": 0,
           "target": "0-tocador-y-lociones"
         },
         {
-          "yaw": 3.1158879647047115,
-          "pitch": 0.09363663617999407,
+          "yaw": 1.6423816370565767,
+          "pitch": 0.16339520572812027,
           "rotation": 0,
           "target": "2-vestidos-y-esposo"
         }
@@ -118,10 +118,116 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.827848928786299,
-          "pitch": 0.11706460259501483,
+          "yaw": 2.3245337681738585,
+          "pitch": 0.311512394029263,
+          "rotation": 5.497787143782138,
+          "target": "3-central-sillon"
+        },
+        {
+          "yaw": -0.6238061291301733,
+          "pitch": 0.12496050404340231,
           "rotation": 0,
-          "target": "1-zapatero-y-bolsos"
+          "target": "4-central-joyas"
+        },
+        {
+          "yaw": -2.700236358349777,
+          "pitch": 0.12711559948606777,
+          "rotation": 0.7853981633974483,
+          "target": "1-zapatero"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-central-sillon",
+      "name": "Central Sillon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18282871347030571,
+          "pitch": 0.3017606599289877,
+          "rotation": 13.351768777756625,
+          "target": "2-vestidos-y-esposo"
+        },
+        {
+          "yaw": -1.3614695110232429,
+          "pitch": 0.09299552595390814,
+          "rotation": 5.497787143782138,
+          "target": "1-zapatero"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-central-joyas",
+      "name": "Central Joyas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.01141333023054436,
+          "pitch": 0.4144063234541058,
+          "rotation": 0,
+          "target": "2-vestidos-y-esposo"
+        },
+        {
+          "yaw": 0.5215918234966139,
+          "pitch": 0.0761168541266457,
+          "rotation": 0.7853981633974483,
+          "target": "2-vestidos-y-esposo"
+        },
+        {
+          "yaw": -0.09287672563253402,
+          "pitch": 0.024893598301439113,
+          "rotation": 5.497787143782138,
+          "target": "3-central-sillon"
         }
       ],
       "infoHotspots": []
